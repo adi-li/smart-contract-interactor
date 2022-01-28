64433181046947f1356e8678f00ac10834793606
Adi <adi-li@users.noreply.github.com>
2022-01-28 17:02:37 +0800

    Add bool field

--- src/utils/parseInputValue.ts
+++ src/utils/parseInputValue.ts
@@ -1,5 +1,10 @@
 import { toBN } from 'web3-utils';
 
+const convertBoolean = (value: string) => {
+  if (value === 'true') return true;
+  return Boolean(parseInt(value, 10));
+};
+
 export default function parseInputValue(input: HTMLInputElement) {
   const { dataType } = input.dataset as { dataType: string };
   if (dataType.startsWith('uint') || dataType.startsWith('int')) {
@@ -12,6 +17,10 @@ export default function parseInputValue(input: HTMLInputElement) {
     return length <= 32 ? parseInt(input.value, 10) : toBN(input.value);
   }
   switch (dataType) {
+    case 'bool':
+      return convertBoolean(input.value);
+    case 'bool[]':
+      return input.value.split(',').map((val) => convertBoolean(val.trim()));
     default:
       return input.value;
   }
